5ae422c248184f8c7bb9fded4983b8a45dc83594
AA21232 <144646591+AA21232@users.noreply.github.com>
2023-10-12 15:32:26 -0400

    Update A#1.java

--- A#1.java
+++ A#1.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String programmerName = "Adam";
+        Scanner input = new Scanner(System.in); // Create a scanner to read user input
+        String programmerName = "Adam";  // Store the programmer's name as "Adam"
         
-        System.out.print("Enter your name: ");
-        String userName = input.nextLine();
+        System.out.print("Enter your name: ");  // Prompt the user to enter their name
+        String userName = input.nextLine();   // Read the user's name from input
         
         if (userName.equals(programmerName)) {
-            System.out.println("We have the same name!");
+            System.out.println("We have the same name!");  // If the user's name matches the programmer's name, display this message
         
         } else {
-            System.out.println("Your name is different from mine!");
+            System.out.println("Your name is different from mine!");  // If the names don't match, display this message
         }
     }
 }
