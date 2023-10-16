e1347009b8239005fa122b3d42b1e4194d362751
AA21232 <144646591+AA21232@users.noreply.github.com>
2023-10-16 12:59:24 -0400

    Update B#4.java

--- B#4.java
+++ B#4.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your age: ");
+        System.out.print("Enter your age: "); // tells the user to input their age
         int age = scanner.nextInt();
 
-        if (age < 13) {
+        if (age < 13) {  // if your age is less than 13 than you are a child 
             System.out.println("You are a child.");
         } else if (age >= 13 && age <= 19) {
-            System.out.println("You are a teen.");
+            System.out.println("You are a teen.");   // if your age is less than or equal to 19 and greater than or equal to 13 you are a teen 
         } else if (age >= 20 && age <= 65) {
-            System.out.println("You are an adult.");
+            System.out.println("You are an adult.");    // if your age is greater than or equal to 20 and less than or equal to 65 you are an adult
         } else {
             System.out.println("You are a senior citizen.");
         }
